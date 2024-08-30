e239a910b2afffb77e9682abf3d1dcbb45b09d4b
Luca Baffa <47544021+lb803@users.noreply.github.com>
2024-08-30 18:09:17 +0100

    add TODO to the Feedback object docstring

--- cfa_api/schemas/feedback.py
+++ cfa_api/schemas/feedback.py
@@ -4,7 +4,15 @@ from typing import Optional
 from pydantic import BaseModel, Field, PositiveInt
 
 class Feedback(BaseModel):
-    """Base model for the Feedback object schema"""
+    """Base model for the Feedback object schema
+    
+    :todo: Whenever this info would be available, we could
+        be more precise when defining types for: `product_id`
+        and `comment`.
+
+        I.e.: is `product_id` an hex number? How many digits
+        long? Or is it an UUID? Pydantic can help validate that.
+    """
     creation_date: datetime
 
     product_id: str

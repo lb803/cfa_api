1278e1c75e9b5d69246b33f82a75b2667fb19dd6
Luca Baffa <47544021+lb803@users.noreply.github.com>
2024-08-31 15:32:38 +0100

    add extract_keywords method to the CommentsAnalyser class, plus unit tests

--- cfa_api/lib/comments_analyser.py
+++ cfa_api/lib/comments_analyser.py
@@ -9,4 +9,15 @@ class CommentsAnalyser:
     """Class to perform analysis operations on feedback comments"""
     def __init__(self, feedbacks: list[Feedback]) -> None:
         self.feedbacks = feedbacks
-
+    
+    def extract_keywords(self, extractor: KeywordExtractor,
+                         limit: int = 5) -> Optional[list[str]]:
+        """Method to extract keywords from comments
+        
+        :param extractor: KeywordExtractor (sub)class to use to perform the
+            keyword extraction.
+        :param limit: Max number of words to extract
+        """
+        comments = ' '.join(feedback.comment for feedback in self.feedbacks \
+                            if feedback.comment is not None)
+        return extractor.extract_keywords(comments, limit)
--- tests/test_comments_analyser.py
+++ tests/test_comments_analyser.py
@@ -6,8 +6,13 @@ from cfa_api.lib.comments_analyser import CommentsAnalyser
 
 
 class MockFeedback:
-    def __init__(self, comment: int = "apple pear"):
+    def __init__(self, comment: int = "apple pear") -> None:
         self.comment = comment
+
+
+class MockKeyworkExtractor:
+    def extract_keywords(*args, **kwargs) -> list[str]:
+        return ["all", "good"]
 
 
 @pytest.mark.parametrize("feedbacks",
@@ -16,3 +21,10 @@ def test_comments_analyser_init(feedbacks) -> None:
     analyser = CommentsAnalyser(feedbacks)
 
     assert analyser.feedbacks == feedbacks
+
+
+def test_comments_analyser_extract_keywords() -> None:
+    analyser = CommentsAnalyser([])
+    words = analyser.extract_keywords(MockKeyworkExtractor(), 5)
+
+    assert words == ["all", "good"]
